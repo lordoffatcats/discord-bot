6244aa15e477e089b92ae06b60b46c693859c352
lordoffatcats <atlielvarson@gmail.com>
2020-05-02 16:15:47 +0000

    fixed word cencorship

--- index.js
+++ index.js
@@ -6,30 +6,30 @@ const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
-let forbiddenWords = ['nigga', 'nigger', 'beggi', 'negri', 'geir', 'eiður', 'hamood', 'habibi', 'Nigger'];
-let valgerður = ['valgerður', 'Valgerður']
-
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', message => {
-  for (var i = 0; i < forbiddenWords.length; i++) {
-    if (message.content.includes(forbiddenWords[i])) {
-      message.channel.send(`stop saying bad words ${message.author.username}`);
-      console.log(`Removed ${message.author.username}'s Message as it had a forbidden word in it.`);
-      message.delete();
-      break;
-    }
-  }
-});
+var SIMPS = ['óskar', 'sjonni', 'pétur']
 
 bot.on('message', message => {
-  for (var i = 0; i < valgerður.length; i++) {
-    if (message.content.includes(valgerður[i]) && message.author.username != 'stór heili maður/ chat filter') {
-      message.channel.send(`Did you mean hvalgerður. ${message.author.username}`);
-      console.log(`${message.author.username}just said valgerdur`);
-      break;
-    }
-  }
-});
+  if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
+    message.channel.send(`stop saying bad words ${message.author.username}`);
+    console.log(`Removed ${message.author.username}'s Message as it had a forbidden word in it.`);
+    message.delete()
+    break;
+  }})
+
+bot.on('message', message => {
+  if(config.VALGERDUR.some(word => message.content.toLowerCase().includes(word))){
+    message.channel.send(`did you mean hvalgerður ${message.author.username}`);
+    console.log(`${message.author.username} said valgerður`);
+    break;
+  }})
+
+bot.on('message', message => {
+  const randomSimp = SIMPS[Math.floor(Math.random() * array.length)];
+  if(config.SIMP.some(word => message.content.toLowerCase().includes(word))){
+    message.channel.send(`did you mean ${randomSimp}`);
+    break;
+  }})
