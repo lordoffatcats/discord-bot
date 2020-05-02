e8ccc7a80619a249b5f32f0bd9dfdd8ff6516399
lordoffatcats <62671111+lordoffatcats@users.noreply.github.com>
2020-05-02 16:52:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-var SIMPS = ['óskar', 'sjonni', 'pétur', 'jói']
+var SIMPS = ['óskar', 'sjonni', 'pétur', 'jóhannes']
 
 bot.on('message', message => {
   if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
